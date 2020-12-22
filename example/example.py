975b73ed16fbb95e0dd14722e6d49ee6d95f0c6f
Raymond Tukpe <rtukpe@gmail.com>
2020-12-22 10:11:15 +0100

    Add example module

---
+++ example/example.py
@@ -0,0 +1,17 @@
+def add(first, second):
+    """
+    :param first: an integer
+    :param second: an integer
+    :returns: The sum of the two numbers
+    """
+    pass
+
+
+def power(number, exponent):
+    """
+    :param number: a positive integer
+    :param exponent: a positive integer
+    :returns: The power of the number to the exponent
+    :raises: TypeError if the number or exponent are less than 0
+    """
+    pass
