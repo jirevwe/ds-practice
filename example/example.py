8c8b4f1a0f88e01609efe3a4dc07811e9911625c
emmanueltukpe <tukpeemmanuel@gmail.com>
2020-12-24 20:18:17 +0100

    set conditions in which test could be carried out

--- example/example.py
+++ example/example.py
@@ -4,7 +4,9 @@ def add(first, second):
     :param second: an integer
     :returns: The sum of the two numbers
     """
-    pass
+    if second == None:
+        raise TypeError('Supply all parameters')
+    return first + second
 
 
 def power(number, exponent):
@@ -14,4 +16,6 @@ def power(number, exponent):
     :returns: The power of the number to the exponent
     :raises: TypeError if the number or exponent are less than 0
     """
-    pass
+    if exponent < 0 or number < 0:
+        raise TypeError('The number must be more than 0')
+    return number**exponent
